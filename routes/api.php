ec99b1a3f990df3f73a0896df3ba5bf24d4c5d20
Romansev <sevenfolder@ya.ru>
2022-02-24 23:29:13 +0300

    conf route env

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CounterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/increment', [CounterController::class, 'increment']);
+
+Route::get('/getAll', [CounterController::class, 'getAllVisitsCount']);
